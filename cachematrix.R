f67eba05607b3a1fc320e1c1b747393feaad5734
MAlakshmi <lakshmi.muddana@gitam.edu>
2014-08-21 16:38:31 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+#Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+x<-matrix(x,n,n)
+get<-function() { x }
+set(y) {x<<-y 
+        }
 
 }
 
